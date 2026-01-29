1dff3548d6bdae82836e084772141845f398851a
ANGEL LUIS CALDERON DEL ROSARIO <angelcalderon687@gmail.com>
2026-01-28 22:31:25 -0400

    Proyecto Base consola

--- ISW123-ReporteroDigital[2024-0380]/Program.cs
+++ ISW123-ReporteroDigital[2024-0380]/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Reportero Digital iniciado...");
+        Console.WriteLine("Reportero Digital iniciado....");
     }
 }
